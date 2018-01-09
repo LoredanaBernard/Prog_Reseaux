af7f0e01307de5ecc7baaab3520bc0509b767bec
Unknown <loredana.bernard.etu@univ-lemans.fr>
2018-01-09 11:43:46 +0100

    envoieMetadonnees 2

--- Serveur/src/ServeurThread.java
+++ Serveur/src/ServeurThread.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class ServeurThread extends Thread{
 	private Socket connectionSocket;
@@ -31,21 +32,56 @@ public class ServeurThread extends Thread{
 		}
 	}
 	
+//	public void recupererMetadonnees (File fichier) {
+//		String chemin = fichier.getPath();
+//		String name = fichier.getName();
+//		long lastModif =  fichier.lastModified();
+//		long sizeFile  = fichier.length();
+//	}
 
-	public void envoiMetadonnees(String repertoireSource) {
+	public File Metadonnees(String repertoireSource) {
+		File metadonnees = new File();
 		String pathName=repertoireSource;
 		File [] fichiersRep;
+		ArrayList<File> aAnalyser = new ArrayList<File>();
+		ArrayList<File> dejaAnalyse = new ArrayList<File>();
+		boolean resteFichier = true;
 		
+		String parent;
 		File fichier = new File(pathName);
 		String chemin = fichier.getPath();
 		String name = fichier.getName();
 		long lastModif =  fichier.lastModified();
 		long sizeFile  = fichier.length();
 		
+		// Si le chemin désigne un dossier 
 		if (fichier.isDirectory()) {
-		// Récupère les fichiers contenus dans le dossier
-		fichiersRep = fichier.listFiles();
+			while (resteFichier) {
+				// Récupère les fichiers contenus dans ce dossier
+				fichiersRep = fichier.listFiles();
+				// Ajout des fichiers du dossier dans une arrayList de fichiers à analyser 
+				for ( int i=0 ; i<fichiersRep.length ; i ++) {
+					aAnalyser.add(fichiersRep[i]);
+				}
+				for (File f : aAnalyser) {
+					if (f.isDirectory()) {
+						
+					}
+				}
+				// S'il n'y a plus de dossier ou fichier à analyser dans le dossier
+				parent = fichier.getParent();
+				
+				
+				// S'il n'y a plus de dossiers et fichiers qui n'ont pas été analysés 
+				resteFichier = false;
+			}		
 		}
 		
+		else {
+			// Envoi des métadonnées du fichier 
+			
+		}
+		
+	return metadonnees;	
 	}
 }
