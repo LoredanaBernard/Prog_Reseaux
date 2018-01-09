f94091a8ce9d2d88ba65ca475bf335595fed8912
Unknown <loredana.bernard.etu@univ-lemans.fr>
2018-01-09 10:21:46 +0100

    Methode recuperationArgs Esclave

--- src/SyncEsclave.java
+++ src/SyncEsclave.java
@@ -11,6 +11,22 @@ public class SyncEsclave {
 	DataOutputStream outToServer;
 	Socket clientSocket;
 	
+	String serveurNom;
+	String serveurPort;
+	int numPort;
+	String repertoireCible;
+	String repertoireSource;
+	
+	// Récupère les données en argument
+	public void recuperationArg(String[] args) {
+		serveurNom = args[0];
+		serveurPort = args[1];
+		numPort = Integer.parseInt(serveurPort);
+		repertoireCible = args[2];
+		repertoireSource = args[3];
+				
+	}
+	
 	public SyncEsclave(int port){
 		initialize(port);
 	}
@@ -19,6 +35,7 @@ public class SyncEsclave {
 		clientSocket.close();
 	}
 
+	// Envoi de requete au serveur
 	public String envoyerRequete(String requete){
 		
 		String recieved="null";
@@ -31,6 +48,7 @@ public class SyncEsclave {
 		return recieved;
 	}
 
+	// Initialisation du client
 	public void initialize(int port){
 		try{
 			inFromUser = new BufferedReader( new InputStreamReader(System.in));
@@ -50,6 +68,12 @@ public class SyncEsclave {
 		catch(IOException e){
 		}
 	}
+	
+	//public boolean comparerMetadonnes() {
+		
+//	}
+	
+	
 
 
 }
